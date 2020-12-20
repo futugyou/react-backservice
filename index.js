fc0e361a65ee0da76b596bc94d64f82e6e7af2ef
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-12-20 12:42:44 +0000

    add graphql
    add graphql

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import app from './app.js'
 import http from 'http'
 import config from './utils/config.js'
-import logger from './utils/logger.js' 
- 
+import logger from './utils/logger.js'
+import hqlServer from './graphql/ApolloApp.js'
+
+hqlServer.listen().then(({ url }) => {
+   logger.info(`hqlserver running on port ${url}`)
+})
 const server = http.createServer(app)
 app.listen(config.PORT, () => {
    logger.info(`server running on port ${config.PORT}`)
